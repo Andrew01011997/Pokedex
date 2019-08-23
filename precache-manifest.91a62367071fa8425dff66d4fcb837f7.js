8aad6682b9c1c40c00116e71ee2fb08df4fc0c5f
Andrew <Andrew01.01.1997@gmail.com>
2019-08-23 20:41:54 +0300

    Updates

---
+++ precache-manifest.91a62367071fa8425dff66d4fcb837f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a91b2c05deb1c1f6bf89bce3d3dfe55",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "658a5211b5a32e756aaa",
+    "url": "/Pokedex/static/css/main.d17742ea.chunk.css"
+  },
+  {
+    "revision": "863ab1f3df07488cb21f",
+    "url": "/Pokedex/static/js/2.4ccef028.chunk.js"
+  },
+  {
+    "revision": "658a5211b5a32e756aaa",
+    "url": "/Pokedex/static/js/main.6fe2051c.chunk.js"
+  },
+  {
+    "revision": "ab4d5c4bf58e55899011",
+    "url": "/Pokedex/static/js/runtime~main.2c996a43.js"
+  }
+]);
